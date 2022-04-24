9bee2a5524c4fe1e73f4d9a5b4de0a8daccac208
bcygsws <bcygswspring168@gmail.com>
2022-04-25 01:37:01 +0800

    :zap: vue3和vue2中生命周期钩子的对比

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <!-- 点击按钮，销毁Life组件，观察组件销毁的钩子 -->
+    <button @click="isShow">点击按钮，销毁Life组件</button>
+    <Life v-if="flag"></Life>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Life from '../components/life/Life.vue';
+export default defineComponent({
+  name: 'About',
+  // 注册子组件
+  components: {
+    Life
+  },
+  setup() {
+    let flag = ref(true);
+    console.log(flag);
+    function isShow() {
+      flag.value = !flag.value;
+    }
+    return { flag, isShow };
+  }
+});
+</script>
