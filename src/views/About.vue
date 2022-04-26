eae8b03b063df7ac18c9afb3b7c9c875902b54f0
bcygsws <bcygswspring168@gmail.com>
2022-04-26 17:25:59 +0800

    :memo: git push org_vue3

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,18 +5,28 @@
     <button @click="isShow">点击按钮，销毁Life组件</button>
     <Life v-if="flag"></Life>
     <DefineHook></DefineHook>
+    <ToRefs></ToRefs>
+    <ToRefs1></ToRefs1>
+    <!-- ref的另外一个作用：获取标签元素对象 -->
+    <UsingRef></UsingRef>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Life from '../components/life/Life.vue';
 import DefineHook from '../components/hook/DefineHook.vue';
+import ToRefs from '../components/to_refs/ToRefs.vue';
+import ToRefs1 from '../components/to_refs/ToRefs1.vue';
+import UsingRef from '../components/to_refs/UsingRef.vue';
 export default defineComponent({
   name: 'About',
   // 注册子组件
   components: {
     Life,
-    DefineHook
+    DefineHook,
+    ToRefs,
+    ToRefs1,
+    UsingRef
   },
   setup() {
     let flag = ref(true);
