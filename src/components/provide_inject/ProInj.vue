aafa4e18d8eacce63eb4e790b30614e5599c725b
bcygsws <bcygswspring168@gmail.com>
2022-04-27 16:43:39 +0800

    :zap: provide/inject的是用
    provide/inject基础

---
+++ src/components/provide_inject/ProInj.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>十四、provide/inject组孙组件传值</div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  onBeforeMount,
+  onMounted
+} from 'vue';
+export default defineComponent({
+  name: 'ProInj',
+  setup() {
+    console.log('1-开始创建组件-setup');
+    const data = reactive({});
+    onBeforeMount(() => {
+      console.log('2.组件挂载页面之前执行----onBeforeMount');
+    });
+    onMounted(() => {
+      console.log('3.-组件挂载到页面之后执行-------onMounted');
+    });
+    return {
+      ...toRefs(data)
+    };
+  }
+});
+</script>
+<style scoped lang="less"></style>
