fb97b624aaade7ddbb968cbfc980680767d8bcd8
bcygsws <bcygswspring168@gmail.com>
2022-05-02 22:49:06 +0800

    :art: 十、只读和浅只读

--- src/components/readonly/ReadOnlyProperty.vue
+++ src/components/readonly/ReadOnlyProperty.vue
@@ -5,6 +5,13 @@
 </template>
 <script lang="ts">
 import { defineComponent, reactive, readonly, shallowReadonly } from 'vue';
+interface IUser {
+  name: string;
+  age: number;
+  cars: {
+    red?: string;
+  };
+}
 export default defineComponent({
   name: 'ReadOnlyProperty',
   setup() {
@@ -20,7 +27,8 @@ export default defineComponent({
     // user1.name += '==';// error: Cannot assign to 'name' because it is a read-only property.
     // user1.cars.red = '劳斯莱斯';// error: Cannot assign to 'red' because it is a read-only property.
     // 2.浅只读：对象自身的property只读，但是不对嵌套对象的深度进行只读转换
-    const user2 = shallowReadonly(user);
+    // const user2 = shallowReadonly(user);
+    const user2 = shallowReadonly<IUser>(user);
     function changeUser() {
       // Error:Cannot assign to 'name' because it is a read-only property.
       // user1.name+="==";
@@ -29,7 +37,11 @@ export default defineComponent({
       // name是user对象自身的property,是只读的，不能更改
       // user2.name += '==';
       // 浅只读不对嵌套对象的深度进行只读转换，换言之；在shallowReadonly中，嵌套的深度是可读可写的
-      user2.cars.red += '666';
+      // user2.cars.red += '666';
+      // 3.特别注意：浅只读的时候，red属性用接口声明为可选的，才可以删除
+      // The operand of a 'delete' operator must be optional.
+      delete user2.cars.red;
+      console.log(user2);
     }
     return {
       user1,
