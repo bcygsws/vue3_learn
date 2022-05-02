b590d2fb88ba44d7aa15ead11784e96cacf62665
bcygsws <bcygswspring168@gmail.com>
2022-05-02 18:59:09 +0800

    :art: 七
    七、toRefs的使用和ref的另外一个作用

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,40 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <!-- 点击按钮，销毁Life组件，观察组件销毁的钩子 -->
-    <button @click="isShow">点击按钮，销毁Life组件</button>
-    <Life v-if="flag"></Life>
-    <DefineHook></DefineHook>
-    <ToRefs></ToRefs>
-    <ToRefs1></ToRefs1>
-    <!-- ref的另外一个作用：获取标签元素对象 -->
-    <UsingRef></UsingRef>
+    <router-link to="/about/to_refs"
+      >七、在不丢失reactive对象的响应式基础上，分解对象，对象每个属性都是一个ref</router-link
+    >
+    <router-view></router-view>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import Life from '../components/life/Life.vue';
-import DefineHook from '../components/hook/DefineHook.vue';
-import ToRefs from '../components/to_refs/ToRefs.vue';
-import ToRefs1 from '../components/to_refs/ToRefs1.vue';
-import UsingRef from '../components/to_refs/UsingRef.vue';
 export default defineComponent({
-  name: 'About',
-  // 注册子组件
-  components: {
-    Life,
-    DefineHook,
-    ToRefs,
-    ToRefs1,
-    UsingRef
-  },
-  setup() {
-    let flag = ref(true);
-    console.log(flag);
-    function isShow() {
-      flag.value = !flag.value;
-    }
-    return { flag, isShow };
-  }
+  name: 'About'
 });
 </script>
