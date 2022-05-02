3a71d37db3c5ce74422c321c6bbe46483717d44e
bcygsws <bcygswspring168@gmail.com>
2022-05-03 01:15:22 +0800

    :art: customRef响应式数跟踪和页面更新进行显式控制

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import CustomRef from '../components/custom_ref/CustomRef.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/home/custom_ref',
+        component: CustomRef
+      }
+    ]
   },
   {
     path: '/about',
