9f9f7cad63f22a4d6e1fd1ebe4a26d1fccf8ec07
bcygsws <bcygswspring168@gmail.com>
2022-05-21 18:19:11 +0800

    :memo: 二、操作代理数据，影响界面更新

--- src/components/update/Update.vue
+++ src/components/update/Update.vue
@@ -26,10 +26,10 @@ import { defineComponent, reactive } from 'vue';
 export default defineComponent({
   name: 'Update',
   setup() {
-    const obj = {
-      // 方式1：期待使用obj点语法，为user增加一个属性，且更新页面，最终失败，gender显示还是空值，
-      //  而且Proxy代理对象中也没有gender属性；将obj的any类型注解去掉，恢复原状
-      // const obj: any = {
+    // const obj = {
+    // 方式1：期待使用obj点语法，为user增加一个属性，且更新页面，最终失败，gender显示还是空值，
+    //  而且Proxy代理对象中也没有gender属性；将obj的any类型注解去掉，恢复原状
+    const obj: any = {
       name: '张三',
       age: 25,
       wife: {
@@ -42,15 +42,18 @@ export default defineComponent({
     let user = reactive<any>(obj);
     function updatePage() {
       // 方式1：期待使用obj点语法，为user增加一个属性，且更新页面
+      // 为普通对象obj,增加或删除属性，对象的属性值确实发生了改变，但是界面不会更新
+      // 结果：控制台打印的obj对象确实增加了一个属性，gender，但是界面没有更新
       // obj.gender = '男'; // 报错：obj对象上不存在gender属性，为obj添加一个any的类型注解
+      // 结果：控制台打印的 obj对象确实删除了age属性，但是界面没有更新
       // delete obj.age; // age的值没了，目标对象obj中也没有age属性了，但是页面没有更新
+      // console.log(obj);
       // 方式2：使用user的点语法，为user增加gender属性，点击按钮，观察页面性别处是否由空白变为有值;
       // 首先user.gender上也报错，提示类型不匹配的问题，为reactive添加泛型reactive<any>(obj)，这个
       // 报错bug解决；然后，点击按钮，查看性别处，由【空值】变成了【男】，页面更新了，而且打印的代理对象
       // 中user也增加了gender这个键值对，obj目标对象上也有gender这个属性了
-      // user.gender = '男';
-      delete user.age; // age的值没了，目标对象中也没有age属性了，页面更新了
-
+      user.gender = '男';
+      // delete user.age; // age的值没了，目标对象中也没有age属性了，页面更新了
       console.log(user);
     }
 
