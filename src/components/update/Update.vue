d13992981381b78af03d60cfeaeeca536592fac6
bcygsws <bcygswspring168@gmail.com>
2023-01-20 01:17:55 +0800

    :bug: 类型断言和接口

--- src/components/update/Update.vue
+++ src/components/update/Update.vue
@@ -24,13 +24,30 @@ import { defineComponent, reactive } from 'vue';
  * 
  * b.尝试使用泛型或者接口，来去掉any这种类型定义
  */
+// 定义代理对象的接口
+interface IRow{
+ key:string;
+ value:string;
+}
+// 任意名字的可选属性[propName:string]:string
+// 或者固定名字的 可选属性;如：gender?:string
+interface IObj{
+  name:string;
+  age:number;
+  gender?:string;
+  wife:{
+    name:string;
+    age:number;
+    cars:IRow[]
+}
+
 export default defineComponent({
   name: 'Update',
   setup() {
     // const obj = {
     // 方式1：期待使用obj点语法，为user增加一个属性，且更新页面，最终失败，gender显示还是空值，
     //  而且Proxy代理对象中也没有gender属性；将obj的any类型注解去掉，恢复原状
-    const obj: any = {
+    const obj= {
       name: '张三',
       age: 25,
       wife: {
@@ -40,7 +57,7 @@ export default defineComponent({
       }
     };
     // 定义一个代理对象user,目标对象是obj
-    let user = reactive<any>(obj);
+    let user = reactive<IObj>(obj);
     function updatePage() {
       // 方式1：期待使用obj点语法，为user增加一个属性，且更新页面
       // 为普通对象obj,增加或删除属性，对象的属性值确实发生了改变，但是界面不会更新
@@ -57,8 +74,7 @@ export default defineComponent({
       // delete user.age; // age的值没了，目标对象中也没有age属性了，页面更新了
       console.log(user);
     }
-
-ADEFRSV     return { user, updatePage };
+return { user, updatePage };
   }
 });
 </script>
