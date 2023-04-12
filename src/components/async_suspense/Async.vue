9b61e9275c32a82f245e383459de63b3522433e4
bcygsws <bcygswspring168@gmail.com>
2023-04-13 04:03:13 +0800

    :bug: get和post请求的区别和联系

--- src/components/async_suspense/Async.vue
+++ src/components/async_suspense/Async.vue
@@ -23,7 +23,13 @@
  * 2.如果是非Promise对象，把这个非Promise对象作为返回结果
  *
  * 二、回顾原生AJAX的创建过程：
- * https://blog.csdn.net/nilmao/article/details/123894943?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-123894943-blog-127348369.235%5Ev28%5Epc_relevant_recovery_v2&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-123894943-blog-127348369.235%5Ev28%5Epc_relevant_recovery_v2&utm_relevant_index=1
+ * 参考文档1：https://blog.csdn.net/nilmao/article/details/123894943?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-123894943-blog-127348369.235%5Ev28%5Epc_relevant_recovery_v2&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-123894943-blog-127348369.235%5Ev28%5Epc_relevant_recovery_v2&utm_relevant_index=1
+ * post请求的三种编码格式
+ * 参考文档2：https://blog.csdn.net/u013258447/article/details/101107743?spm=1001.2101.3001.6650.4&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4-101107743-blog-118662566.235%5Ev28%5Epc_relevant_recovery_v2&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4-101107743-blog-118662566.235%5Ev28%5Epc_relevant_recovery_v2&utm_relevant_index=5
+ * get请求和post请求的区别：
+ * 参考文档3:https://blog.csdn.net/wang037195/article/details/99585320?spm=1001.2101.3001.6650.3&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-3-99585320-blog-101107743.235%5Ev28%5Epc_relevant_recovery_v2&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-3-99585320-blog-101107743.235%5Ev28%5Epc_relevant_recovery_v2&utm_relevant_index=4
+ *
+ *
  * 2.1 创建ajax对象
  * const xhr=new XMLHttpRequest();
  * 2.2 设置请求方法和请求地址url
