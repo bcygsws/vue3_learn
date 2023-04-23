03e7f6b04bc35e7df63a6b8f8a3bc45a16b5c86f
bcygsws <bcygswspring168@gmail.com>
2023-04-23 23:57:10 +0800

    :art: Promise的使用-深度

--- src/components/async_suspense/Async.vue
+++ src/components/async_suspense/Async.vue
@@ -55,6 +55,7 @@
  * post请求的三种编码格式
  * 参考文档2：https://blog.csdn.net/u013258447/article/details/101107743?spm=1001.2101.3001.6650.4&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4-101107743-blog-118662566.235%5Ev28%5Epc_relevant_recovery_v2&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4-101107743-blog-118662566.235%5Ev28%5Epc_relevant_recovery_v2&utm_relevant_index=5
  *
+ * 
  * Content-Type:application/x-www-form-urlencoded
  * ajax的默认请求格式
  * 请求体中的数据，以json字符串的形式发送到后端
