f39f4848ac608182da6c11ce1e57ea06e57f9fb5
bcygsws <bcygswspring168@gmail.com>
2023-05-05 05:42:49 +0800

    :ambulance: t在vue3中的使用

--- src/types/person.ts
+++ src/types/person.ts
@@ -2,14 +2,16 @@
  * @Description:
  * @Author: bcygsws@163.com
  * @Date: 2023-01-20 13:52:50
- * @LastEditTime: 2023-01-20 14:29:50
- * @FilePath:Web-project\vue3_learn\src\types\person.ts
+ * @LastEditTime: 2023-05-05 03:09:49
+ * @FilePath: \vue_shop_serverd:\Web-project\vue3_learn\src\types\person.ts
  */
 // 任意名字的可选属性[propName:string]:string
 // 或者固定名字的 可选属性;如：gender?:string
+// gender?: string;
 export default interface IObj {
   name: string;
   age: number;
+  // [propName: string]: string;
   gender?: string;
   wife: {
     name: string;
