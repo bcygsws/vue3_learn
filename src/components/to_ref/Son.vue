445c68afe4528a5a397be22c6984907a5d790f5c
bcygsws <bcygswspring168@gmail.com>
2023-05-28 04:43:24 +0800

    :ambulance: toRef定义reactive数据的属性，操作同一数值，共同更新

--- src/components/to_ref/Son.vue
+++ src/components/to_ref/Son.vue
@@ -8,38 +8,40 @@
   <p>length:{{ length }}</p>
 </template>
 <script lang="ts">
-import { defineComponent, toRef, computed, Ref } from "vue";
+import { defineComponent, toRef, computed, Ref } from 'vue';
 // 别人封装的获取长度函数
 // const getLength=computed((age:Ref)=>{})
 function getLength(age: Ref) {
   // 数据age只要变化，就计算长度，需要监听
   // 这种属于只有getter的computed的监听
   return computed(() => {
-    return age.value.toString().length;
+    // number-->string, age.value.toString()方法转换成字符串，字符串或数组才有length属性
+    return age.value.toString().length; // 数值型表达式，number类型；则getLength(age: Ref)的返回值仍然是number类型
   });
 }
 export default defineComponent({
-  name: "Son",
+  name: 'Son',
   props: {
     age: {
       type: Number,
-      required: true, // required关键字声明，必须接收的props属性
+      required: true // required关键字声明，必须接收的props属性
     },
     money: {
       type: Number,
-      required: true,
-    },
+      required: true
+    }
   },
   setup(props) {
     // 要使用传入的age,使用setup的参数props
+    // 打印props，发现props是一个代理对象Proxy{ }，里面包含的是父组件传入，且使用props选项接收的属性
     console.log(props);
     // 别人定义的函数getLength参数接收的是Ref类型数据，props中接收的父组件toRef传递过来的age,是普通数值；
     // 而目前age是number类型数据，因此需要把number类型转换成Ref类型
-    const length = getLength(toRef(props, "age"));
+    const length = getLength(toRef(props, 'age'));
     return {
-      length,
+      length
     };
-  },
+  }
 });
 /**
  *
