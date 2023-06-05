88f1a67585e458f28c6f227ec27b3cd107fc51d1
bcygsws <bcygswspring168@gmail.com>
2023-06-06 03:15:50 +0800

    :sparkles: vue中transition过渡动画

--- src/components/transition/Transition.vue
+++ src/components/transition/Transition.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <h3>1.vue3中transition的使用</h3>
-    <button @click="flag =!flag">点击按钮，切换动画</button>
+    <button @click="flag = !flag">点击按钮，切换动画</button>
     <!-- transition指定name属性后，过渡动画中的类v系列名称，变成fade-enter-from fade-endter-active等等 -->
     <transition name="fade">
       <div v-if="flag" class="box"></div>
@@ -10,6 +10,39 @@
 </template>
 
 <script lang="ts">
+/**
+ *
+ * @一、transition中定义name属性（比如为：str）后，直接在style标签中书写
+ * str-enter-from 
+ * str-enter-active
+ * str-enter-to
+ * 等6种类样式即可
+ * 
+ * 注意：一这种方式，比方式2中定义enter-from-class、enter-active-class、
+ *  enter-to-class、leave-from-class、
+ * leave-active-class、leave-to-class更简洁
+ * 
+ * <transition name="fade">
+ *  <div v-if="flag"></div>
+ * </transition>
+ * 
+ * 进行某种操作，比如点击按钮后，flag由false变为true;则enter这三种类样式工作；
+ * 同样的flag由true变成false时，则leave这三种类样式工作
+ *
+ * v-enter-from
+ * v-enter-active
+ * v-enter-to
+ *
+ * v-leave-from
+ * v-leave-active
+ * v-leave-to
+ * 1.当在transition标签中，声明name属性后；需要在style标签中中定义
+ * fade-enter-from、fade-enter-active、fade-enter-to和
+ * fade-leave-from、fade-leave-active、fade-leave-to 
+ * 这6种类样式
+ *
+ *
+ */
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
   name: 'Transition',
