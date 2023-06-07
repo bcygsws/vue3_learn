f789b4606c06ded382f3dc93917b1bdc86db22f9
bcygsws <bcygswspring168@gmail.com>
2023-06-08 05:43:42 +0800

    :ambulance: 路由和gsap实现跨组件动画

---
+++ src/components/transition/cross-component-animation/Page1.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page1_container">
+    <div class="box">page1</div>
+  </div>
+</template>
+<script></script>
+<style lang="less" scoped>
+.page1_container {
+  .box {
+  }
+}
+</style>
